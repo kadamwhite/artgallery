c2610c40f9db615b173af78dad6759f9dbf09c36
K. Adam White <kadamwhite@users.noreply.github.com>
2019-04-21 21:06:19 -0400

    Upgrade npm deps, webpack config, and eslint/babelrc

--- .config/webpack.config.dev.js
+++ .config/webpack.config.dev.js
@@ -1,27 +1,44 @@
 /**
  * This file defines the configuration for development and dev-server builds.
  */
+const { resolve } = require( 'path' );
 const { externals, helpers, presets } = require( '@humanmade/webpack-helpers' );
 const { choosePort, cleanOnExit, filePath } = helpers;
+
+const pluginPath = ( ...pathParts ) => resolve( __dirname, '..', ...pathParts );
 
 // Clean up manifests on exit.
 cleanOnExit( [
 	filePath( 'build/asset-manifest.json' ),
 ] );
 
-module.exports = choosePort( 9091 ).then( port => presets.development( {
-	devServer: {
-		port,
-	},
-	externals: {
-		...externals,
-		jquery: 'jQuery',
-	},
+const config = presets.development( {
+	externals,
 	entry: {
-		editor: filePath( 'src/index.js' ),
+		editor: pluginPath( 'src/index.js' ),
 	},
 	output: {
-		path: filePath( 'build' ),
-		publicPath: `http://localhost:${ port }/build/`,
+		path: pluginPath( 'build' ),
 	},
-} ) );
+} );
+
+// If this is the top-level Webpack file loaded by the Webpack DevServer,
+// automatically detect & bind to an open port.
+if (
+	process.argv[1].indexOf( 'webpack-dev-server' ) !== -1
+	&& pluginPath( '.config' ) === __dirname
+) {
+	const cwdRelativePublicPath = ( path, port ) => `http://localhost:${ port }${ path.replace( process.cwd(), '' ) }/`;
+	module.exports = choosePort( 9090 ).then( port => presets.development( {
+		...config,
+		devServer: {
+			port,
+		},
+		output: {
+			...config.output,
+			publicPath: cwdRelativePublicPath( config.output.path, port ),
+		},
+	} ) );
+} else {
+	module.exports = presets.development( config );
+}
