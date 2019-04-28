e13308df59ef9cf017d02f9e8682af0b3ce45c8c
K. Adam White <kadamwhite@users.noreply.github.com>
2019-04-27 20:35:12 -0400

    Move upgrade routine firing to wp_loaded action

--- inc/namespace.php
+++ inc/namespace.php
@@ -22,10 +22,20 @@ function setup() {
 	Blocks\setup();
 	Scripts\setup();
 
-	// Check the installed version of the plugin and run upgrade procedures if
-	// the current version does not match the installed version.
+	add_action( 'wp_loaded', __NAMESPACE__ . '\\maybe_run_upgrade_routine' );
+}
+
+/**
+ * Check to see if the installed version of this plugin matches the current code-
+ * specified version number, and fire the hook to run plugin upgrade procedures if
+ * the current version does not match the version in the database.
+ *
+ * @return void
+ */
+function maybe_run_upgrade_routine() {
 	$option_key = 'artgallery_plugin_version';
 	$installed_version = get_option( $option_key );
+
 	if ( $installed_version !== ARTGALLERY_VERSION ) {
 		update_option( $option_key, ARTGALLERY_VERSION );
 		do_action( 'artgallery_upgrade' );
