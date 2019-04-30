3b48beb1da112b4c42712a71420563813b03353a
K. Adam White <kadamwhite@users.noreply.github.com>
2019-04-30 17:51:59 -0400

    Remove unused commented-out code

--- src/blocks/artwork-grid/index.js
+++ src/blocks/artwork-grid/index.js
@@ -1,17 +1,9 @@
 import { __ } from '@wordpress/i18n';
-// import { Fragment } from '@wordpress/element';
-// import { RadioControl } from '@wordpress/components';
 import { ServerSideRender } from '@wordpress/editor';
-// import { withDispatch, withSelect } from '@wordpress/data';
-
-// import { AVAILABILITY_TAXONOMY } from '../../constants';
-// import { bemBlock } from '../../utils';
 
 import Icon from './icon';
 
 export const name = 'artgallery/artwork-grid';
-
-// const block = bemBlock( 'artwork-grid' );
 
 export const options = {
 	title: __( 'Artwork Grid', 'artgallery' ),
