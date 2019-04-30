e52ae4fc79e82e488246d171f11e5df76621a38d
K. Adam White <kadamwhite@users.noreply.github.com>
2019-04-30 12:04:29 -0400

    Improve logic of featured post force-selection

--- src/blocks/core-image/index.js
+++ src/blocks/core-image/index.js
@@ -1,11 +1,39 @@
-import { compose, createHigherOrderComponent } from '@wordpress/compose';
-import { withDispatch, withSelect } from '@wordpress/data';
+import { createHigherOrderComponent } from '@wordpress/compose';
+import { dispatch, select, subscribe, withSelect } from '@wordpress/data';
 
 import {
 	ARTWORK_POST_TYPE,
 } from '../../constants';
 
 export const name = 'core/image';
+
+// /**
+//  * Listen for store updates, and detect artwork items which have an image block
+//  * but lack an assigned featured image. For any matching block, automatically
+//  * set the populated image block's image as the featured media.
+//  */
+// subscribe( () => {
+// 	const { getEditedPostAttribute, getBlocks } = select( 'core/editor' );
+// 	if ( getEditedPostAttribute( 'type' ) !== ARTWORK_POST_TYPE ) {
+// 		return;
+// 	}
+
+// 	if ( getEditedPostAttribute( 'featured_media' ) ) {
+// 		return;
+// 	}
+
+// 	const imageBlock = getBlocks().find( block => block.name === name );
+// 	if ( ! imageBlock || ! imageBlock.attributes.id ) {
+// 		return;
+// 	}
+
+// 	// If this is an image block, if this post is an Artwork item, if there
+// 	// is no existing featured image, AND if the image for this block has
+// 	// been set, then automatically feature that image.
+// 	dispatch( 'core/editor' ).editPost( {
+// 		featured_media: imageBlock.attributes.id,
+// 	} );
+// } );
 
 export const filters = [
 	/**
@@ -14,35 +42,24 @@ export const filters = [
 	{
 		hook: 'editor.BlockEdit',
 		namespace: `artgallery/${ name }`,
-		callback: createHigherOrderComponent( BlockEdit => compose(
-			withSelect( select => {
-				const editor = select( 'core/editor' );
-				return {
-					isArtworkItem: editor.getEditedPostAttribute( 'type' ) === ARTWORK_POST_TYPE,
-					featuredMediaId: editor.getEditedPostAttribute( 'featured_media' ),
-				};
-			} ),
-			withDispatch( dispatch => ( {
-				setFeaturedMediaId: id => dispatch( 'core/editor' ).editPost( {
-					featured_media: id,
-				} ),
-			} ) ),
-		)( ( {
-			isArtworkItem,
-			featuredMediaId,
-			setFeaturedMediaId,
-			...props
-		} ) => {
-			// If this is an image block, if this post is an Artwork item, if there
-			// is no existing featured image, AND if the image for this block has
-			// been set, then automatically feature that image.
-			if ( props.name === 'core/image' && isArtworkItem && ! featuredMediaId && props.attributes.id ) {
-				setFeaturedMediaId( props.attributes.id );
+		callback: createHigherOrderComponent( BlockEdit => withSelect( ( select, ownProps ) => {
+			// Only proceed for image blocks with an assigned ID.
+			if ( ownProps.name === 'core/image' && ownProps.attributes.id ) {
+				const { getEditedPostAttribute } = select( 'core/editor' );
+
+				if ( getEditedPostAttribute( 'type' ) === ARTWORK_POST_TYPE ) {
+
+					if ( ! getEditedPostAttribute( 'featured_media' ) ) {
+						// If this is an image block, if this post is an Artwork item, if there
+						// is no existing featured image, AND if the image for this block has
+						// been set, then automatically feature that image.
+						dispatch( 'core/editor' ).editPost( {
+							featured_media: ownProps.attributes.id,
+						} );
+					}
+				}
 			}
-			// Return the BlockEdit component for use as normal.
-			return (
-				<BlockEdit { ...props } />
-			);
-		} ), 'autoFeatureFirstSelectedImage' ),
+			return null;
+		} )( BlockEdit ), 'autoFeatureFirstSelectedImage' ),
 	},
 ];
